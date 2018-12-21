c14015292a0043b751345838f5924de954f68d8f
JuM1697 <justin.michael@marquardt.de>
2018-12-21 14:17:10 +0100

    Using arguments or pre defined variables plus debug if not entered

--- check_nextcloud_plugin_updates.sh
+++ check_nextcloud_plugin_updates.sh
@@ -1,15 +1,62 @@
 #!/bin/bash
 #Author: Justin Michael
-#Date: 18.12.18
-#Version: 1.1
 #Requirements:
 #	- sudo permissions for nagios user
-#	- Nextcloud user (called "icinga") with admin permissions 
-
+#	- nextcloud user with admin permissions
 set -eu
 nextcloud_user=""
 nextcloud_password=""
 nextcloud_url=""
+
+while getopts ":u:p:a:" opt; do
+	case $opt in
+		u)
+			nextcloud_user=$OPTARG
+			;;
+		p)
+			nextcloud_password=$OPTARG
+			;;
+		a)
+			nextcloud_url=$OPTARG
+			;;
+		\?)
+			echo "Invalid option: -$OPTARG"
+			exit 5
+			;;
+		:)
+			echo "Option -$OPTARG requires an argument."
+			exit 5
+			;;
+	esac
+done
+
+
+if [ -z $nextcloud_user ] && [ -z $nextcloud_password ] && [ -z $nextcloud_url ]
+then
+	echo "Either provide a user with -u argument and a password with -p argument and an url with -a argument or define them in line #7, #8 and #9"
+	exit 5
+fi
+
+if [ -z $nextcloud_user ]
+then
+	echo "Either provide a user with -u argument or define one in line #7"
+	exit 5
+fi
+
+if [ -z $nextcloud_password ]
+then
+	echo "Either provide a password with -p argument or define one in line #8"
+	exit 5
+fi
+
+
+if [ -z $nextcloud_url ]
+then
+	echo "Either provide an url with -a argument or define one in line #9"
+	exit 5
+fi
+
+
 
 sudo -u www-data /var/www/nextcloud/occ user:enable $nextcloud_user > /dev/null 
 
