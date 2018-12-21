5b906fc92eddf5e075118843d9b9c5ddd6d28f17
JuM1697 <justin.michael@marquardt.de>
2018-12-21 13:49:21 +0100

    defined variables for userdata plus URL

--- check_nextcloud_plugin_updates.sh
+++ check_nextcloud_plugin_updates.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 #Author: Justin Michael
 #Date: 18.12.18
-#Version: 1.0
+#Version: 1.1
 #Requirements:
 #	- sudo permissions for nagios user
 #	- Nextcloud user (called "icinga") with admin permissions 
 
-sudo -u www-data /var/www/nextcloud/occ user:enable icinga > /dev/null
+set -eu
+nextcloud_user=""
+nextcloud_password=""
+nextcloud_url=""
 
-num_updates="$(curl -s --user #username#:#password# #url# | grep num_updates_availabl | sed 's/[^0-9]*//g')"
+sudo -u www-data /var/www/nextcloud/occ user:enable $nextcloud_user > /dev/null 
 
-if [ $num_updates -gt 0 ]
+num_updates=`curl -s --user $nextcloud_user:$nextcloud_password $nextcloud_url | grep num_updates_availabl | sed 's/[^0-9]*//g'`
+
+if [ $num_updates -ne 0 ]
 then
 	echo "Nextcloud Plugins CRITICAL - $num_updates updates are available"
 	sudo -u www-data /var/www/nextcloud/occ user:disable icinga > /dev/null
